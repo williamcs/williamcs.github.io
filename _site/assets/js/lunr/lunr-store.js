09059a206990392f652d43a84fc316416246fa27
William Zhu <william.zhu@nexusguard.com>
2018-11-01 15:44:10 +0800

    deleted _site folder

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Hello Test: welcome",
-        "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
-        "tags": ["machine learning","data science","neural network"],
-        "url": "http://localhost:4000/hello-test/",
-        "teaser":null}]
